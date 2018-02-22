8f34ed0c6993490e9e36cdc2dd9771a8ce300a1e
Pooya Soleimany <pooya@Pooyas-MBP.hsd1.ca.comcast.net>
2018-02-22 01:05:20 -0800

    Abrevated state and obfoscated ssn

--- homeworks/03-python/python-challenge/PyBoss/main.py
+++ homeworks/03-python/python-challenge/PyBoss/main.py
@@ -14,28 +14,87 @@
 # * Import the `employee_data1.csv` and `employee_data2.csv` files, which currently 
 #   holds employee records like the below:
 # ```
-# Emp ID,Name,DOB,SSN,State
-# 214,Sarah Simpson,1985-12-04,282-01-8166,Florida
-# 15,Samantha Lara,1993-09-08,848-80-7526,Colorado
-# 411,Stacy Charles,1957-12-20,658-75-8526,Pennsylvania
+# Emp ID,   Name,           DOB,        SSN,            State
+# 214,      Sarah Simpson,  1985-12-04, 282-01-8166,    Florida
+# 15,       Samantha Lara,  1993-09-08, 848-80-7526,    Colorado
+# 411,      Stacy Charles,  1957-12-20, 658-75-8526,    Pennsylvania
 # ```
 
 # * Then convert and export the data to use the following format instead:
 # ```
-# Emp ID,First Name,Last Name,DOB,SSN,State
-# 214,Sarah,Simpson,12/04/1985,***-**-8166,FL
-# 15,Samantha,Lara,09/08/1993,***-**-7526,CO
-# 411,Stacy,Charles,12/20/1957,***-**-8526,PA
+# Emp ID,   First Name,     Last Name,  DOB,        SSN,            State
+# 214,      Sarah,          Simpson,    12/04/1985, ***-**-8166,    FL
+# 15,       Samantha,       Lara,0      9/08/1993,  ***-**-7526,    CO
+# 411,      Stacy,          Charles,    12/20/1957, ***-**-8526,    PA
 # ```
 
-# * In summary, the required conversions are as follows:
+class Converter:
+    _us_state_abbrev = {
+        'Alabama': 'AL',
+        'Alaska': 'AK',
+        'Arizona': 'AZ',
+        'Arkansas': 'AR',
+        'California': 'CA',
+        'Colorado': 'CO',
+        'Connecticut': 'CT',
+        'Delaware': 'DE',
+        'Florida': 'FL',
+        'Georgia': 'GA',
+        'Hawaii': 'HI',
+        'Idaho': 'ID',
+        'Illinois': 'IL',
+        'Indiana': 'IN',
+        'Iowa': 'IA',
+        'Kansas': 'KS',
+        'Kentucky': 'KY',
+        'Louisiana': 'LA',
+        'Maine': 'ME',
+        'Maryland': 'MD',
+        'Massachusetts': 'MA',
+        'Michigan': 'MI',
+        'Minnesota': 'MN',
+        'Mississippi': 'MS',
+        'Missouri': 'MO',
+        'Montana': 'MT',
+        'Nebraska': 'NE',
+        'Nevada': 'NV',
+        'New Hampshire': 'NH',
+        'New Jersey': 'NJ',
+        'New Mexico': 'NM',
+        'New York': 'NY',
+        'North Carolina': 'NC',
+        'North Dakota': 'ND',
+        'Ohio': 'OH',
+        'Oklahoma': 'OK',
+        'Oregon': 'OR',
+        'Pennsylvania': 'PA',
+        'Rhode Island': 'RI',
+        'South Carolina': 'SC',
+        'South Dakota': 'SD',
+        'Tennessee': 'TN',
+        'Texas': 'TX',
+        'Utah': 'UT',
+        'Vermont': 'VT',
+        'Virginia': 'VA',
+        'Washington': 'WA',
+        'West Virginia': 'WV',
+        'Wisconsin': 'WI',
+        'Wyoming': 'WY',
+    }
+    # * In summary, the required conversions are as follows:
 
-#   * The `Name` column should be split into separate `First Name` and `Last Name` columns.
+    #   * The `Name` column should be split into separate `First Name` and `Last Name` 
+    #     columns.
 
-#   * The `DOB` data should be re-written into `DD/MM/YYYY` format.
+    #   * The `DOB` data should be re-written into `DD/MM/YYYY` format.
 
-#   * The `SSN` data should be re-written such that the first five numbers are hidden from view.
+    #   * The `SSN` data should be re-written such that the first five numbers are 
+    #     hidden from view.
+    def obfuscate_ssn(self, ssn):
+        "***-**-" + ssn.rpartition('-')[2]
 
-#   * The `State` data should be re-written as simple two-letter abbreviations.
-
-# * Special Hint: You may find this link to be helpful—[Python Dictionary for State Abbreviations](https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5).
+    #   * The `State` data should be re-written as simple two-letter abbreviations.    
+    # * Special Hint: You may find this link to be helpful—[Python Dictionary for State 
+    #   Abbreviations](https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5).
+    def get_state_abbrevation(self, state_name):
+        return Converter._us_state_abbrev[state_name]
